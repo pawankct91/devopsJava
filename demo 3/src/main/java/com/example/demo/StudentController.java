1c4820a1f7f224ec2d6864069aa1a2f2f726d06d
pawankct91 <pawankct91@gmail.com>
2018-12-23 09:48:26 +0530

    Add files via upload

---
+++ demo 3/src/main/java/com/example/demo/StudentController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+
+	@GetMapping("/data")
+	public String getInfo() {
+		return "Pawan Sharma";
+	}
+	
+
+
+}
